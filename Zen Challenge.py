53d0e655f12d982cae002d9b97e5bc0ca9664358
Mykhailo Rogozhyn <imihanick@gmail.com>
2023-01-05 23:09:09 +0200

    Update Zen Challenge.py
    The re rule was changed

--- Zen Challenge.py
+++ Zen Challenge.py
@@ -23,7 +23,7 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 """
 
-matchObj = re.findall('[a-zA-Z]*y\b', zen)
+matchObj = re.findall('\b[a-zA-Z]*y\b', zen)
 
 if matchObj:
     print(matchObj)
